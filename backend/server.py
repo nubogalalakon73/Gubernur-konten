08bd9fabbd0225fad0b750ccb8e460a97b1b9f80
nubogalalakon73 <pt.segala@gmail.com>
2026-05-20 21:47:07 +0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -624,6 +624,7 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://gubernurkonten.vercel.app"],
     allow_credentials=True,
     allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
